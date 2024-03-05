8fd10a1aeef68f8d2452ed7d0e7d7971c1be28bc
Aleksey Sobolev <iammabus@gmail.com>
2024-03-05 18:43:54 +0300

    feat: complete getDayName and getNextFriday tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -46,8 +46,17 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  return weekDays[new Date(date).getDay()];
 }
 
 /**
@@ -61,8 +70,11 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  do {
+    date.setDate(date.getDate() + 1);
+  } while (date.getDay() !== 5);
+  return date;
 }
 
 /**
